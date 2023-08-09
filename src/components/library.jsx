a779f2d4f03a9d73f77c966ce65afbf89687b35c
King-Windows <krsazon@gmail.com>
2023-08-08 20:50:54 -0700

    Adds Function

---
+++ src/components/library.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const library = () => {
+  return (
+    <div>library</div>
+  )
+}
+
+'use strict'
+
+let myLibrary = [];
+
+function Book(){
+
+}
+
+function addBookToLibrary(){
+
+}
+
+
+
+export default library
