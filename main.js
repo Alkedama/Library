2afeadcfcf34deff278497e7124c5a3012a655d3
King-Windows <krsazon@gmail.com>
2023-08-09 16:58:38 -0700

    Adds Button Function

--- main.js
+++ main.js
@@ -1,14 +1,63 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
 import './assets/style.scss'
 
 document.querySelector('#app').innerHTML = `
   <div>
+    <button id="btnAdd">Add</button>
+    <button id="btnView">View</button>
 
+    <div id="bookContainer"></div>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+'use strict'
+
+let myLibrary = [];
+
+function Book(title, author, pages, read){
+  this.title = title,
+  this.author = author,
+  this.pages = pages,
+  this.read = read
+}
+
+Book.prototype.info = function(){
+  if(this.read === true){
+    return `{ Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}, Read: Yes }`;
+  } else {
+  return `{ Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}, Read: No }`;
+  }
+}
+
+function addBookToLibrary(book){
+  myLibrary.push(book);
+}
+
+let output = new Book('Magna Carta', 'Rudra', 100, false);
+
+output.info();
+
+const btnAdd = document.querySelector('#btnAdd');
+const btnView = document.querySelector('#btnView');
+
+const bookContainer = document.querySelector('#bookContainer');
+
+const lblAdd = document.createElement('div');
+const lblView = document.createElement('div')
+
+btnAdd.addEventListener('click', () => {
+  bookContainer.appendChild(lblAdd);
+
+  console.log('clickAdd');
+  addBookToLibrary(output);
+  lblAdd.textContent = output.info();
+});
+
+btnView.addEventListener('click', () => {
+  bookContainer.appendChild(lblView);
+
+  console.log('clickView');
+  let firstObject = myLibrary[0];
+
+  console.log(firstObject.author);
+  lblView.textContent = firstObject;
+});
